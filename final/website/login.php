fc603fe02ced030ca9e8ade374291b0b20e058b9
junavel <22104768@usc.edu.ph>
2023-11-13 20:03:28 +0800

    update

--- final/website/login.php
+++ final/website/login.php
@@ -1,7 +1,5 @@
 <?php
-
 session_start();
-
 include_once("included/dbutil.php");
 
 $conn = getConnection();
@@ -13,23 +11,24 @@ $sql = "SELECT * from access where email = '".$email."' and password = '".$passw
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 
-if ($row["email"] == $email && $row["password"]== $password){ 
-     if ($row["role"] == "admin") {
-         $_SESSION["user_role"] = "admin"; 
+if ($row["email"] == $email && $row["password"]== $password) {
+    // Login successful
+    $_SESSION["user_id"] = $row["user_id"]; // Assuming you have a user ID
+    $_SESSION["user_full_name"] = $row["user_full_name"]; // Replace with the actual user's full name column name
+    $_SESSION["user_job"] = $row["user_job"]; // Replace with the actual user's job column name
+    // Add other user-related session variables as needed
 
-         header("Location: Admin/index.html");
-
-        } else {
-
-            $_SESSION["user_role"] = "user";
-
-            header("Location: User/index.html");
-
-        }
+    if ($row["role"] == "admin") {
+        $_SESSION["user_role"] = "admin";
+        header("Location: Admin/index.html");
+    } else {
+        $_SESSION["user_role"] = "user";
+        header("Location: User/index.html");
+    }
 } else {
+    // Login failed
     header("Location: index.php");
 }
 
 closeConnection();
-
 ?>
